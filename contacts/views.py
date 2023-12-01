2c11ebd670c463cbd392fb61469b15c0382eba2f
qureshi-hassanahmed <qureshi.hassanahmed@gmail.com>
2023-12-02 03:30:06 +0500

    list page

--- contacts/views.py
+++ contacts/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.views.generic import ListView, CreateView, DetailView, UpdateView, DeleteView
+from django.urls import reverse_lazy
 
-# Create your views here.
+from .models import Contact
+from .test_form import ContactForm # in tutorial this was named contact_form.py ~ contact_form -- fix this later
+
+class ContactListView(ListView):
+    model = Contact
+    template_name = 'contacts/contact_list.html'
--- hassan_qureshi/settings.py
+++ hassan_qureshi/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'hassan_qureshi.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / "templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
