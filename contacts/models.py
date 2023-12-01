e3d09a5eb866b4aae674e7fee29547afb2c97bd8
qureshi-hassanahmed <qureshi.hassanahmed@gmail.com>
2023-12-02 02:01:09 +0500

    adding model

--- contacts/models.py
+++ contacts/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class Contact(models.Model):
+    name = models.CharField(null = False, max_length=50, unique=True)
+    notes = models.TextField(max_length=200, default='')
+    created_at = models.DateTimeField(auto_now_add=True)
+    email = models.EmailField(null=False, unique=True)
