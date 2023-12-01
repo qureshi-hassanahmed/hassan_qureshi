efda22d8927889c2eb06a95c874332cac00d681d
qureshi-hassanahmed <qureshi.hassanahmed@gmail.com>
2023-12-02 01:47:02 +0500

    initial commit

---
+++ hassan_qureshi/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for hassan_qureshi project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'hassan_qureshi.settings')
+
+application = get_wsgi_application()
